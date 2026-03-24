e6ef91a2668a08c0e4a279098f433100608fdc86
Victor Melquiades Leite <VictorMsL@academicos.com>
2026-03-24 20:53:19 -0300

    aula dia 24

--- src/main/java/br/uniesp/si/techback/controller/FuncionarioController.java
+++ src/main/java/br/uniesp/si/techback/controller/FuncionarioController.java
@@ -3,10 +3,8 @@ package br.uniesp.si.techback.controller;
 import br.uniesp.si.techback.model.Funcionario;
 import br.uniesp.si.techback.service.FuncionarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +15,26 @@ public class FuncionarioController {
 
     private final FuncionarioService service;
 
+    @GetMapping
+    public List<Funcionario> listar(){
+        return service.listar();
+    }
+
     @PostMapping
-    public Funcionario Salvar(Funcionario funcionario){
+    public Funcionario salvar(Funcionario funcionario){
         return service.Salvar(funcionario);
     }
 
-    @GetMapping
-    public List<Funcionario> Listar(){
-        return service.Listar();
+    @PutMapping("/{id}")
+    public Funcionario atualizar(@PathVariable Long id,
+                                 @RequestBody Funcionario funcionario){
+        return service.atualizar(id, funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Funcionario> excluir(@PathVariable Long id){
+        service.excluir(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/java/br/uniesp/si/techback/service/FuncionarioService.java
+++ src/main/java/br/uniesp/si/techback/service/FuncionarioService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor //cria um construtor apenas para as variaveis q estao marcada como final
@@ -13,13 +14,34 @@ public class FuncionarioService {
 
     private final FuncionarioRepository repository;
 
+    public List<Funcionario> listar(){
+        return repository.findAll();
+    }
+
     public Funcionario Salvar(Funcionario funcionario){
         return repository.save(funcionario);
     }
 
-    public List<Funcionario> Listar(){
-        return repository.findAll();
+    public Funcionario atualizar(Long id, Funcionario funcionario){
+        Optional<Funcionario> func = repository.findById(id);
+        if(func.isEmpty()){
+            throw new RuntimeException("Funcionario inexistente");
+        } else {
+            return repository.save(funcionario);
+        }
     }
+
+
+
+    public void excluir(Long id){
+        if(!repository.existsById(id)){
+            throw new RuntimeException("Id não encontrado");
+        }
+        repository.deleteById(id);
+    }
+
+
+
 
 
 }
